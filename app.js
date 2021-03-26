893808d2ba63d4198ca1795f1e8942311faed0c0
jinsu1 <jinsu4205@gmail.com>
2021-03-26 14:45:28 +0900

    JS saveBtn
    saveFile PaintJS.png

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const penRange = document.getElementById("jsRange");
 const rangeNum = document.getElementById("rangeNum");
-const mode = document.getElementById("jsMode");
+const modeBtn = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const COLOR_black = "#2c2c2c";
 const SIZE_canvas = 550;
@@ -13,6 +14,8 @@ const SIZE_canvas = 550;
 canvas.width = SIZE_canvas;
 canvas.height = SIZE_canvas;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, SIZE_canvas, SIZE_canvas);
 ctx.strokeStyle = COLOR_black;
 ctx.lineWidth = 2.5;
 ctx.fillStyle = COLOR_black;
@@ -49,12 +52,17 @@ function handleCanvasClick() {
     }
  }   
 
+ function handleContextMenu(event) {
+     event.preventDefault();
+ }
+
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleContextMenu);
 }
 
 function changeColor(event) {
@@ -78,13 +86,25 @@ if(penRange) {
 function buttonMode() {
     if(filling === true) {
         filling = false;
-        mode.innerText="Fill";
+        modeBtn.innerText="Fill";
     } else {
         filling = true;
-        mode.innerText="Paint";  
+        modeBtn.innerText="Paint";  
     }
 }
 
-if(mode) {
-    mode.addEventListener("click", buttonMode);
+if(modeBtn) {
+    modeBtn.addEventListener("click", buttonMode);
+}
+
+function saveFile() {
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS";
+    link.click();
+}
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", saveFile);
 }
